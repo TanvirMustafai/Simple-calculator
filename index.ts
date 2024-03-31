2e6e4f72848d1096b424711349c2f1755056b1e1
Tanvir Mustafai <tanvirgujjar7@gmail.com>
2024-04-01 03:11:25 +0500

    My first

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message: "Enter your firstNumber",type :"number", name: "firstNumber"},
+    {message: "Enter your secondNumber",type :"number", name: "secondNumber"},
+    {
+        message: "Select one of operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "SUBTRACTION", "multiplication", "DIVISION"],
+    },
+]);
+
+// Conditional Statment
+if (answer.operator === "ADDITION") {
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "SUBTRACTION") {
+    console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}else if (answer.operator === "DIVISION") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {"Please select a valid operator}"}
+
+
+    
